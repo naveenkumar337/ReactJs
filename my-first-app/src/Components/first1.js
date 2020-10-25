4d1afb30efe8f4cb89954a52f4c5a332bce22459
naveenkumar337 <naveenajay337@gmail.com>
2020-10-25 11:41:53 +0530

    'demo'

--- my-first-app/src/Components/first1.js
+++ my-first-app/src/Components/first1.js
@@ -3,11 +3,10 @@ import React from 'react'
 export default function first1() {
     return (
         <div>
-<<<<<<< HEAD
+
             <h1>welcome from file Demo checking conflicts   </h1>
-=======
-            welcome file
->>>>>>> e540775... 'commit'
+
+          
         </div>
     )
 }
