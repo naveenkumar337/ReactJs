341a2c1033caf6f0e5f4bba8bba1e7c1f97d08b0
naveenkumar337 <naveenajay337@gmail.com>
2020-10-25 11:35:34 +0530

    code commited

--- my-first-app/src/Components/first1.js
+++ my-first-app/src/Components/first1.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function first1() {
     return (
         <div>
+<<<<<<< HEAD
             <h1>welcome from file Demo checking conflicts   </h1>
+=======
+            welcome file
+>>>>>>> e540775... 'commit'
         </div>
     )
 }
