41a1ef137c3a79f118349fc3abbafad371b564a6
naveenkumar337 <naveenajay337@gmail.com>
2020-10-25 10:53:31 +0530

    'changes'

--- my-first-app/src/Components/first1.js
+++ my-first-app/src/Components/first1.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function first1() {
     return (
         <div>
-            <h1>welcome to file Demo    </h1>
+            <h1>welcome from file Demo    </h1>
         </div>
     )
 }
