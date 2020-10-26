27df31f2e0303933ae95f46c05ad2a3ee6063fa4
naveenkumar337 <naveenajay337@gmail.com>
2020-10-26 21:19:26 +0530

    deleted unwanted files

--- my-first-app/src/Components/first1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function first1() {
-    return (
-        <div>
-            <h1>welcome from file Demo checking conflicts   </h1>
-        </div>
-    )
-}
